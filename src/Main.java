503c935a05906ee198908f69618c27345f617688
lokytech5 <lokytech5@gmail.com>
2023-09-01 19:54:54 +0100

    Updated main method for user interaction

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         //Creating list of Books
         var book1 = new Book("JavaProgramming","programming",
                 "peter Waverly", "ISB-JA10981", true);
@@ -37,41 +38,65 @@ public class Main {
         library.setAvailable(book2.isAvailable());
         library.setAvailable(book3.isAvailable());
 
-        library.removeBook(book1);
-
         //registering patrons to the library
         library.registerPatrons(patron1);
         library.registerPatrons(patron2);
         library.registerPatrons(patron3);
 
-        // Displaying available books
-        System.out.println("Available Books:");
-        library.listAvailableBooks();
-        System.out.println();
+        System.out.println("Welcome to the Library Management System!");
 
-        //Displaying available patron
-        library.listPatrons();
-        System.out.println();
+        while(true) {
+            System.out.println("Main Menu:");
+            System.out.println("1. List all available books");
+            System.out.println("2. List registered patrons"); // Added option to list patrons
+            System.out.println("3. Borrow a book");
+            System.out.println("4. Return a book");
+            System.out.println("5. Exit");
+            System.out.println();
 
-       //stimulating book borrowing
-        patron1.borrowBook(book1);
-        patron2.borrowBook(book2);
-        patron3.borrowBook(book3);
+            System.out.print("Enter your choice: ");
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    // Displaying available books
+                    System.out.println();
+                    library.listAvailableBooks();
+                    System.out.println();
+                    break;
+                case 2:
+                    //Displaying available patron
+                    System.out.println();
+                    library.listPatrons();
+                    System.out.println();
+                    break;
+                case 3:
+                    //stimulating book borrowing
+                    System.out.println("Borrowing books:");
+                    patron1.borrowBook(book1);
+                    patron2.borrowBook(book2);
+                    patron3.borrowBook(book3);
+                    break;
+                case 4:
+                    //stimulating returning book borrowing
+                    System.out.println("Returning Books");
+                    patron1.returnedBook(book1);
+                    patron2.returnedBook(book2);
+                    break;
+                case 5:
+                    System.out.println("Exiting the Library Management System.");
+                    return; // Exit the program
+                default:
+                    System.out.println("Invalid choice. Please select a valid option.");
+            }
+        }
 
-        //stimulating returning book borrowing
-        System.out.println();
-        patron1.returnedBook(book1);
-        patron2.returnedBook(book2);
-
-        //Patron2 is trying to borrow book1.
-
-        // Simulate borrowing a book by a patron
-        System.out.println("Borrowing a book by James:");
-        library.borrowedBook(patron1, book1);
-
-        // Simulate returning a book by a patron
-        System.out.println("Returning a book by James:");
-        library.returnedBook(patron1, book1);
+//        // Simulate borrowing a book by a patron
+//        System.out.println("Borrowing a book by James:");
+//        library.borrowedBook(patron1, book1);
+//
+//        // Simulate returning a book by a patron
+//        System.out.println("Returning a book by James:");
+//        library.returnedBook(patron1, book1);
 
     }
     }
